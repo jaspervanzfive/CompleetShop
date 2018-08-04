456a5766cb4868af73f068793fccdba732e5dd81
softlair <softlair@outlook.com>
2018-08-05 07:42:55 +0800

    Add delete on products

--- CompleetKassa.ViewModels/Commands/BaseCommand.cs
+++ CompleetKassa.ViewModels/Commands/BaseCommand.cs
@@ -1,30 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Input;
 
 namespace CompleetKassa.ViewModels.Commands
 {
+
+    /// <summary>
+    /// A command whose sole purpose is to 
+    /// relay its functionality to other
+    /// objects by invoking delegates. The
+    /// default return value for the CanExecute
+    /// method is 'true'.
+    /// </summary>
     public class BaseCommand : ICommand
     {
-        private Action<object> _method;
-        public event EventHandler CanExecuteChanged;
+        #region Fields
 
-        public BaseCommand(Action<object> method)
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+
+        #endregion // Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new command that can always execute.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public BaseCommand(Action<object> execute)
+            : this(execute, null)
         {
-            _method = method;
         }
 
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public BaseCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        #endregion // Constructors
+
+        #region ICommand Members
+
+        [DebuggerStepThrough]
         public bool CanExecute(object parameter)
         {
-            return true;
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
         }
 
         public void Execute(object parameter)
         {
-            _method.Invoke(parameter);
+            _execute(parameter);
         }
+
+        #endregion // ICommand Members
     }
 }
